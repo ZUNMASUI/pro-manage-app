2875a1ff19f5e892d2337dd1f62094cadf5c459c
ZUNMASUI <katyou6340@gmail.com>
2021-02-19 01:03:49 +0900

    最低限機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
 
-  # 先ほど追加したルーティングは削除する
-  # そしてこの行を追加する
-  resources :list, only: %i(new create edit update destroy )
+  resources :list, only: %i(new create edit update destroy) do
+    resources :card, except: %i(index)
+  end
 end
